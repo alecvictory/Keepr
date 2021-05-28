4d418cecc5e88fd2b8a9a04d89167ce61442bcab
alec victory <alecvictory93@gmail.com>
2021-05-28 11:13:50 -0600

    first commit

--- Keepr.client/src/env.js
+++ Keepr.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const baseURL = dev ? 'http://localhost:5001' : ''
+export const domain = 'dev-xy5n17w4.us.auth0.com'
+export const audience = 'https://Keepr.com'
+export const clientId = 'vmr3g6umRT9ejUECAII3hjVyxVX3nc8p'
