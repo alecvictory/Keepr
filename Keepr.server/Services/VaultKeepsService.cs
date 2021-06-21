44d9163a3b28a70773d8d765e15f9e69bf5dd7a5
alec victory <alecvictory93@gmail.com>
2021-06-21 11:37:34 -0600

    code documentation

--- Keepr.server/Services/VaultKeepsService.cs
+++ Keepr.server/Services/VaultKeepsService.cs
@@ -13,11 +13,12 @@ namespace Keepr.server.Services
         {
             _vk = vk;
         }
-
+        // NOTE this function is passed to the repository to get all VaultKeeps
         internal List<VaultKeepViewModel> GetVaultKeeps(int id)
         {
             return _vk.GetVaultKeeps(id);
         }
+        // NOTE this function is passed to the repository and it is getting a vaultkeep by it's ID. If the vaultkeep does not exist it'll throw an error.
         internal VaultKeepViewModel GetVaultKeepById(int id)
         {
             VaultKeepViewModel v = _vk.GetVaultKeepById(id);
@@ -32,7 +33,7 @@ namespace Keepr.server.Services
         {
             return _vk.CreateVaultKeep(vk);
         }
-
+        // NOTE this function is passed to the repository to create a vaultkeep
         internal void RemoveVaultKeep(int id, string creatorId)
         {
             VaultKeepViewModel vk = _vk.GetVaultKeepById(id);
--- Keepr.server/Services/VaultsService.cs
+++ Keepr.server/Services/VaultsService.cs
@@ -14,12 +14,12 @@ namespace Keepr.server.Services
         {
             _vr = vr;
         }
-
+        // NOTE this function is passed to the repository to get all vaults
         internal List<Vault> GetAllVaults()
         {
             return _vr.GetAllVaults();
         }
-
+        // NOTE this function is passed to the repository and it is getting a vault by it's ID. If the vault does not exist it'll throw an error.
         internal Vault GetVaultById(int id)
         {
             var v = _vr.GetVaultById(id);
@@ -30,13 +30,13 @@ namespace Keepr.server.Services
 
             return v;
         }
-
+        // NOTE this function is passed to the repository to create a vault
         internal Vault CreateVault(Vault v)
         {
             return _vr.CreateVault(v);
         }
-
-        internal Vault UpdateVault(Vault v, string id)
+        // NOTE this function is passed to the repository to edit a vault. The function will get the vault by an ID if the vault does not exist it'll throw an error. It will check if the creator ID matches the user ID, if it doesn't it'll throw an error. 
+        internal Vault UpdateVault(Vault v, string userId)
         {
             Vault vault = _vr.GetVaultById(v.Id);
 
@@ -44,13 +44,13 @@ namespace Keepr.server.Services
             {
                 throw new Exception("Invalid Id");
             }
-            if (vault.CreatorId != id)
+            if (vault.CreatorId != userId)
             {
                 throw new Exception("This is not your Vault to edit");
             }
             return _vr.UpdateVault(v);
         }
-
+        // NOTE this function is passed to the repository to delete a vault. It will first get the vault by it's Id then it will check if the creator ID matches the user ID. If it does not then it will throw an error.
         internal void RemoveVault(int id, string userId)
         {
             Vault vault = GetVaultById(id);
@@ -61,7 +61,7 @@ namespace Keepr.server.Services
             }
             _vr.Remove(id);
         }
-
+        // NOTE this function is passed to the repository to get all vaults by profile Id
         internal List<Vault> GetVaultsByProfileId(string id, string userInfoId)
         {
             if (id == userInfoId)
